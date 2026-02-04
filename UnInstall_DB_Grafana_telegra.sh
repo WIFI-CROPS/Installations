335cb3d9a2f282fc7098dbcc0ccf95a014ab202b
AT TECH THAILAND <wifi.crops@gmail.com>
2026-02-04 15:13:01 +0700

    Add uninstallation script for Grafana and Telegraf

---
+++ UnInstall_DB_Grafana_telegra.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Downloading script..."
+wget -q https://raw.githubusercontent.com/WIFI-CROPS/Tools/refs/heads/main/Ubantu_Server/UnInstall_DB_Grafana_telegraf.txt -O UnInstall_DB_Grafana_telegraf.sh
+sed -i 's/\r$//' UnInstall_DB_Grafana_telegraf.sh
+chmod +x UnInstall_DB_Grafana_telegraf.sh
+echo "Starting Uninstallation..."
+./UnInstall_DB_Grafana_telegraf.sh
+echo "Cleaning up..."
+rm UnInstall_DB_Grafana_telegraf.*
+echo "Done! UnInstallation complete"
