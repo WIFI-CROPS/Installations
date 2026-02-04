be7b702e5bec709d2927e180b02f408f777698be
AT TECH THAILAND <wifi.crops@gmail.com>
2026-02-04 15:30:42 +0700

    Update UnInstall_DB_Grafana_telegraf.sh

--- UnInstall_DB_Grafana_telegraf.sh
+++ UnInstall_DB_Grafana_telegraf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Downloading script..."
-wget -q https://raw.githubusercontent.com/WIFI-CROPS/Tools/refs/heads/main/Ubantu_Server/Install_DB_Grafana_telegraf.txt -O Install_DB_Grafana_telegraf.sh
-sed -i 's/\r$//' Install_DB_Grafana_telegraf.sh
-chmod +x Install_DB_Grafana_telegraf.sh
-echo "Starting installation..."
-./Install_DB_Grafana_telegraf.sh
+wget -q https://raw.githubusercontent.com/WIFI-CROPS/Tools/refs/heads/main/Ubantu_Server/UnInstall_DB_Grafana_telegraf.txt -O UnInstall_DB_Grafana_telegraf.sh
+sed -i 's/\r$//' UnInstall_DB_Grafana_telegraf.sh
+chmod +x UnInstall_DB_Grafana_telegraf.sh
+echo "Starting Uninstallation..."
+./UnInstall_DB_Grafana_telegraf.sh
 echo "Cleaning up..."
-rm Install_DB_Grafana_telegraf.*
-echo "Done! Installation complete"
+rm UnInstall_DB_Grafana_telegraf.*
+echo "Done! UnInstallation complete"
