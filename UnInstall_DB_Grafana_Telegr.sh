fff542fac252a78efa39b13fdd7079c09f0f8819
AT TECH THAILAND <wifi.crops@gmail.com>
2026-02-04 16:00:34 +0700

    Modify uninstallation script to remove all .sh files
    Updated cleanup process to remove all shell scripts.

--- UnInstall_DB_Grafana_Telegr.sh
+++ UnInstall_DB_Grafana_Telegr.sh
@@ -6,6 +6,5 @@ chmod +x UnInstall_DB_Grafana_telegraf.sh
 echo "Starting Uninstallation..."
 ./UnInstall_DB_Grafana_telegraf.sh
 echo "Cleaning up..."
-rm UnInstall_DB_Grafana_Telegr.sh
-rm UnInstall_DB_Grafana_telegraf.sh
+rm *.sh
 echo "Done! UnInstallation complete"
